36d099845598da4973dc4d161cec9f6370af3f4e
zhanghy <zhjx2015@aliyun.com>
2018-07-20 17:08:00 +0800

    commit

--- dev/src/main/java/com/dev/base/login/controller/LoginController.java
+++ dev/src/main/java/com/dev/base/login/controller/LoginController.java
@@ -8,6 +8,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhjx on 2018/7/17.
@@ -25,10 +29,21 @@ public class LoginController {
 //    Init init;
 
     @RequestMapping("/")
-    public String login(){
-//        System.out.println(init.getUrl());
-//        System.out.println(init.getUserList().get(2).getName());
-//        System.out.println(init.getConfigMap().get("name"));
-        return "index";
+    public ModelAndView login(ModelAndView view){
+        view.addObject("key","123445");
+        User u = new User();
+        u.setName("this is my user's name");
+        view.addObject("user",u);
+        view.addObject("url","baiduoo.com");
+        view.addObject("id",1223);
+        List<String> strList = new ArrayList<>();
+        strList.add("12444");
+        strList.add("12444");
+        strList.add("12444");
+
+        view.addObject("notEmptyList",strList);
+        view.addObject("emptyList",new ArrayList<>());
+        view.setViewName("index");
+        return view;
     }
 }
